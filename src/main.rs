4083287035af57e16f9d69e7826e39c63bb3a67b
Masoud <masoud67.md@gmail.com>
2025-12-19 09:19:57 +0330

    type command path check

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,61 @@
+use std::env;
+use std::fs;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
 
 fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
+
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
         let command = input.split_ascii_whitespace().next().unwrap();
         let args = input.split_once(command).unwrap().1.trim();
 
+        let valid_commands = ["exit", "echo", "type"];
+
         match command {
             "exit" => break,
             "echo" => println!("{}", args),
-            "type" => match args {
-                "exit" | "echo" | "type" => println!("{args} is a shell builtin"),
-                _ => println!("{}: not found", args),
-            },
+            "type" => handle_type_command(&valid_commands, args),
             _ => println!("{}: command not found", input.trim()),
         }
+    }
+}
+
+fn handle_type_command(valid_commands: &[&str], args: &str) {
+    match valid_commands.contains(&args) {
+        true => println!("{args} is a shell builtin"),
+        false => match env::var_os("PATH") {
+            Some(paths) => {
+                for path in env::split_paths(&paths) {
+                    match fs::read_dir(path) {
+                        Ok(results) => {
+                            for result in results {
+                                match result {
+                                    Ok(res) => {
+                                        if res.path().is_file()
+                                            && res.metadata().unwrap().permissions().mode() & 0o111
+                                                != 0
+                                            && res.file_name() == args
+                                        {
+                                            println!("{args} in {:?}", res.path());
+                                            continue;
+                                        }
+                                    }
+                                    Err(_) => println!("Error finding file"),
+                                }
+                            }
+                        }
+                        Err(_) => println!("Error reading dir"),
+                    }
+                }
+            }
+            None => (),
+        },
+        false => println!("{args}: not found"),
     }
 }
